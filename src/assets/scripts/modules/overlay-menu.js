401f18d65642d6b68fa84e24c96ea117fe9e7f71
Roman Lickevich <r.lickevich@gmail.com>
2019-01-17 00:19:27 +0200

    overlay menu

---
+++ src/assets/scripts/modules/overlay-menu.js
@@ -0,0 +1,21 @@
+function overlay() {
+
+    const heroMenu = document.querySelector('.hero__menu');
+    const overlayMenu = document.querySelector('.overlay-menu');
+    const menuNavItems = document.querySelectorAll('.menu-nav__item--overlay');
+    const navTrigger = document.querySelector('.nav-trigger');
+
+    heroMenu.addEventListener('click', toggleClass);
+
+    for (const iterator of menuNavItems) {
+        iterator.addEventListener('click', toggleClass)
+    }
+
+    function toggleClass(e) {
+        e.preventDefault();
+
+        overlayMenu.classList.toggle('is-active');
+        navTrigger.classList.toggle('is-active');
+    }
+}
+overlay();
