5b5d9ca7f4149481c5ba751f4052bd42e3ff1b2b
Roman Lickevich <r.lickevich@gmail.com>
2019-01-17 21:43:29 +0200

    parallax 50%

--- src/assets/scripts/modules/overlay-menu.js
+++ src/assets/scripts/modules/overlay-menu.js
@@ -1,21 +1,17 @@
-function overlay() {
 
-    const heroMenu = document.querySelector('.hero__menu');
-    const overlayMenu = document.querySelector('.overlay-menu');
-    const menuNavItems = document.querySelectorAll('.menu-nav__item--overlay');
-    const navTrigger = document.querySelector('.nav-trigger');
+const heroMenu = document.querySelector('.hero__menu');
+const overlayMenu = document.querySelector('.overlay-menu');
+const menuNavItems = document.querySelectorAll('.menu-nav__item--overlay');
+const navTrigger = document.querySelector('.nav-trigger');
 
-    heroMenu.addEventListener('click', toggleClass);
+heroMenu.addEventListener('click', toggleClass);
 
-    for (const iterator of menuNavItems) {
-        iterator.addEventListener('click', toggleClass)
-    }
-
-    function toggleClass(e) {
-        e.preventDefault();
-
-        overlayMenu.classList.toggle('is-active');
-        navTrigger.classList.toggle('is-active');
-    }
+for (const iterator of menuNavItems) {
+    iterator.addEventListener('click', toggleClass)
 }
-overlay();
+
+function toggleClass() {
+    
+    overlayMenu.classList.toggle('is-active');
+    navTrigger.classList.toggle('is-active');
+}
