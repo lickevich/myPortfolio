0df309b1aa407a3e9176362775d9b0d5747f77fc
Roman Lickevich <r.lickevich@gmail.com>
2019-01-18 13:34:04 +0200

    welcome auth

---
+++ src/assets/scripts/modules/welcome.js
@@ -0,0 +1,17 @@
+const user = document.querySelector('.user-welcome');
+const auth = document.querySelector('.auth');
+const authLink = document.querySelector('.welcome__auth');
+const buttons = document.querySelector('.auth__form__buttons');
+const button = buttons.firstElementChild;
+
+authLink.addEventListener('click', toggleClass);
+
+function toggleClass(e) {
+    e.preventDefault();
+
+    user.classList.toggle('is-active');
+    auth.classList.toggle('is-active');
+    authLink.classList.toggle('is-active');
+}
+
+button.addEventListener('click', toggleClass);
